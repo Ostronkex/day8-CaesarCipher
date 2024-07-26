00177dfd943ac6d68e69af87bf41e402d7f00629
Erik Magnusson <erikmagnusson.privat@gmail.com>
2024-07-26 15:01:13 +0200

    Create auditorium-21.py

---
+++ auditorium-21.py
@@ -0,0 +1,17 @@
+Temporary placeholder
+
+# Write your code below this line 👇
+n = int(input())
+number = n
+
+for num in range(1, number):
+  if number % num == 0 and number % 1 == 0:
+    print("Prime")
+  else: 
+    print("Not prime")
+  
+# Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input()) # Check this number
+prime_checker(number=n)
